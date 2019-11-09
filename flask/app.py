87251b51cce90fdf304ee08d8cc4c383e04e7e26
ronan <g00342393@gmit.ie>
2019-11-09 16:16:43 +0000

    Got basic AJAX post request working

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return app.send_static_file("digit-classifier.html")
 
+
+@app.route("/classify", methods=["POST"])
+def classify():
+    print(request.form)
+    return "Test"
 
 if __name__ == '__main__':
     app.run()
