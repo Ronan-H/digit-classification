8f66aa95567a661bf34618e73970d20d62e55f5c
ronan <g00342393@gmit.ie>
2019-11-10 09:48:14 +0000

    Classification sent back to HTTP client

--- flask/app.py
+++ flask/app.py
@@ -26,13 +26,12 @@ def classify():
     pixel_data = request.form.getlist("pixelData[]")
     # convert boolean array to values from 0-255
     pixel_nums = np.array([255 if v == "true" else 0 for v in pixel_data], dtype=np.uint8)
-    model_input = pixel_nums.reshape((150, 150))
-    image = Image.fromarray(model_input).resize([28, 28], Image.ANTIALIAS)
+    model_input = pixel_nums.reshape((250, 250))
+    image = Image.fromarray(model_input).resize([28, 28], Image.NEAREST)
     model_input = np.array(image).reshape((1, 28, 28, 1))
-    # image.save("test_image.png")
+    image.save("test_image.png")
     model_output = model.predict_classes(model_input)
-    print("PREDICTION: ", model_output)
-    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+    return str(model_output[0])
 
 
 if __name__ == '__main__':
