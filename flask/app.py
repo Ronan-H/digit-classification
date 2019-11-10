8c269948f666c11cb6936e30c81bd8d76bfa12e6
ronan <g00342393@gmit.ie>
2019-11-10 14:28:10 +0000

    Cleaned up Flask code and added comments

--- flask/app.py
+++ flask/app.py
@@ -1,108 +1,41 @@
 from flask import Flask
 from flask import request
-import json
-import numpy as np
 import keras
-from PIL import Image
-from PIL import ImageOps
+
+import image_ops
+
 app = Flask(__name__)
-
-
-def load_model():
-    model = keras.models.load_model("../model/model.h5")
-    print("Loaded model from disk")
-    return model
-
-
-model = load_model()
-
-
-def scale_and_center(img, img_size, scaled_size):
-    immat = img.load()
-    crop_left = img_size
-    crop_right = 0
-    crop_up = img_size
-    crop_down = 0
-    for x in range(img_size):
-        for y in range(img_size):
-            if immat[(x, y)] != 0:
-                crop_left = min(x, crop_left)
-                crop_right = max(x, crop_right)
-                crop_up = min(y, crop_up)
-                crop_down = max(y, crop_down)
-
-    cropped = img.crop((crop_left, crop_up, crop_right + 1, crop_down + 1))
-    cropped.save("cropped.png")
-    if cropped.size[0] > cropped.size[1]:
-        expand_amount = (cropped.size[0] - cropped.size[1]) // 2
-        cropped = ImageOps.expand(cropped, border=(0, expand_amount, 0, expand_amount))
-    else:
-        expand_amount = (cropped.size[1] - cropped.size[0]) // 2
-        cropped = ImageOps.expand(cropped, border=(expand_amount, 0, expand_amount, 0))
-
-    scaled = cropped.resize([20, 20], Image.NEAREST)
-    cropped.save("resized-20.png")
-    scaled = ImageOps.expand(scaled, border=4)
-
-    # https://stackoverflow.com/questions/37519238/python-find-center-of-object-in-an-image
-    immat = scaled.load()
-    m = np.zeros((scaled_size, scaled_size))
-
-    for x in range(scaled_size):
-        for y in range(scaled_size):
-            m[x, y] = immat[(x, y)] != 0
-
-    s = np.sum(np.sum(m))
-
-    if s == 0:
-        # canvas is blank, do nothing
-        return scaled
-
-    m = m / s
-
-    # marginal distributions
-    dx = np.sum(m, 1)
-    dy = np.sum(m, 0)
-
-    # expected values
-    cx = np.sum(dx * np.arange(scaled_size))
-    cy = np.sum(dy * np.arange(scaled_size))
-
-    middle = scaled_size / 2
-    offset_x = cx - middle
-    offset_y = cy - middle
-
-    # https://stackoverflow.com/questions/37584977/translate-image-using-pil
-    a = 1
-    b = 0
-    c = round(offset_x)  # left/right (i.e. 5/-5)
-    d = 0
-    e = 1
-    f = round(offset_y)  # up/down (i.e. 5/-5)
-    translated = scaled.transform(scaled.size, Image.AFFINE, (a, b, c, d, e, f))
-
-    return translated
+# load model from file
+model = keras.models.load_model("../model/model.h5")
 
 
 @app.route('/')
 def hello():
+    """
+    Serves a page to draw digits on and classify them
+    """
+
     return app.send_static_file("digit-classifier.html")
 
 
 @app.route("/classify", methods=["POST"])
 def classify():
-    scaled_size = 28
+    """
+    Classifies a drawn digit based on pixel data taken from the canvas
+    :return: Digit classification (0-9)
+    """
 
     img_size = int(request.values.get("width"))
     pixel_data = request.form.getlist("pixelData[]")
-    # convert boolean array to values from 0-255
-    pixel_nums = np.array([255 if v == "true" else 0 for v in pixel_data], dtype=np.uint8)
-    model_input = pixel_nums.reshape((img_size, img_size))
-    # image = Image.fromarray(model_input).resize([28, 28], Image.NEAREST)
-    image = Image.fromarray(model_input)
-    image = scale_and_center(image, img_size, scaled_size)
-    model_input = np.array(image).reshape((1, scaled_size, scaled_size, 1))
-    image.save("test_image.png")
+
+    # convert post data to PIL Image
+    canvas_img = image_ops.post_data_to_image(pixel_data, img_size)
+    # crop, resize, and translate the drawn digit to match the MNIST data set as closely as possible
+    mnistified_img = image_ops.mnistify_image(canvas_img)
+    # convert the image to a form that the model accepts as input
+    model_input = image_ops.image_to_model_input(mnistified_img)
+
+    # make and return the digit classification
     model_output = model.predict_classes(model_input)
     return str(model_output[0])
 
---
+++ flask/image_ops.py
@@ -0,0 +1,152 @@
+from flask import Flask
+from flask import request
+import numpy as np
+from PIL import Image
+from PIL import ImageOps
+
+
+def crop_to_digit(img):
+    """
+    Crop image to the exact size of the drawn digit
+    """
+
+    # assume image is square for simplicity
+    img_size = img.size[0]
+
+    # find the edges of the drawn digit (effectively creating a bounding box to crop)
+    crop_left = img_size
+    crop_right = 0
+    crop_up = img_size
+    crop_down = 0
+
+    img_data = img.load()
+    for x in range(img_size):
+        for y in range(img_size):
+            if img_data[(x, y)] != 0:
+                crop_left = min(x, crop_left)
+                crop_right = max(x, crop_right)
+                crop_up = min(y, crop_up)
+                crop_down = max(y, crop_down)
+
+    return img.crop((crop_left, crop_up, crop_right + 1, crop_down + 1))
+
+
+def expand_to_square(img):
+    """
+    Expands an image out into square shape (matches width/height to whichever is bigger)
+    """
+
+    if img.size[0] > img.size[1]:
+        # width bigger than height; expand height
+        expand_amount = (img.size[0] - img.size[1]) // 2
+        cropped = ImageOps.expand(img, border=(0, expand_amount, 0, expand_amount))
+    else:
+        # height bigger than width; expand width
+        expand_amount = (img.size[1] - img.size[0]) // 2
+        cropped = ImageOps.expand(img, border=(expand_amount, 0, expand_amount, 0))
+
+    return cropped
+
+
+def scale_to_mnist(img):
+    """
+    Shrinks an image down to MNIST scale (20x20) and adds padding up to 28x28
+    """
+    scaled = img.resize([20, 20], Image.ANTIALIAS)
+    return ImageOps.expand(scaled, border=4)
+
+
+def center_digit(img):
+    """
+    Centers the digit within the image, based on Center of Mass
+    """
+
+    # https://stackoverflow.com/questions/37519238/python-find-center-of-object-in-an-image
+    img_size = img.size[0]
+    img_data = img.load()
+    m = np.zeros((img_size, img_size))
+
+    for x in range(img_size):
+        for y in range(img_size):
+            m[x, y] = img_data[(x, y)] != 0
+
+    s = np.sum(np.sum(m))
+
+    if s == 0:
+        # canvas is blank, return nothing
+        return None
+
+    m = m / s
+
+    # marginal distributions
+    dx = np.sum(m, 1)
+    dy = np.sum(m, 0)
+
+    # expected values
+    cx = np.sum(dx * np.arange(img_size))
+    cy = np.sum(dy * np.arange(img_size))
+
+    middle = img_size / 2
+    offset_x = cx - middle
+    offset_y = cy - middle
+
+    # https://stackoverflow.com/questions/37584977/translate-image-using-pil
+    a = 1
+    b = 0
+    c = round(offset_x)  # left/right (i.e. 5/-5)
+    d = 0
+    e = 1
+    f = round(offset_y)  # up/down (i.e. 5/-5)
+
+    return img.transform(img.size, Image.AFFINE, (a, b, c, d, e, f))
+
+
+def post_data_to_image(pixel_data, img_size):
+    """
+    Converts data from the post form into a PIL Image
+    """
+
+    # convert boolean array to values from 0-255
+    pixel_nums = np.array([255 if v == "true" else 0 for v in pixel_data], dtype=np.uint8)
+    # reshape into 2d array
+    reshaped = pixel_nums.reshape((img_size, img_size))
+    return Image.fromarray(reshaped)
+
+
+def mnistify_image(img):
+    """
+    Converts the canvas image into an image that resembles a digit from the MNIST
+    data set as closely as possible. This is broken down into 4 steps:
+
+    1. Crop image to digit
+    2. Extend image out into a square
+    3. Scale the image down to 20x20, and expand edges out to 28x28
+    4. Center digit within image based on Center of Mass
+    """
+
+    img.save("debug/0-canvas-img.png")
+
+    cropped = crop_to_digit(img)
+    cropped.save("debug/1-cropped.png")
+
+    squared = expand_to_square(cropped)
+    squared.save("debug/2-squared.png")
+
+    scaled = scale_to_mnist(squared)
+    scaled.save("debug/3-scaled.png")
+
+    centered = center_digit(scaled)
+    centered.save("debug/4-centered-final.png")
+
+    return centered
+
+
+def image_to_model_input(img):
+    """
+    Converts a 28x28 PIL image to data that can be used as input for the Keras model
+    """
+
+    img_size = img.size[0]
+
+    model_input = np.array(img).reshape((1, img_size, img_size, 1))
+    return model_input
